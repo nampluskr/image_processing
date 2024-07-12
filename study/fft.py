f2b261f37dcc397b1d736fa67274f0c4ef0e29ab
nampluskr <37031766+nampluskr@users.noreply.github.com>
2024-07-12 15:40:39 +0900

    Create fft.py

---
+++ study/fft.py
@@ -0,0 +1,84 @@
+import numpy as np
+from scipy import fft as fft2d
+from test.image import Image, Function
+
+
+#####################################################################
+## Math fucntions
+
+class Log10(Function):
+    def forward(self, x):
+        return np.log10(x)
+
+class Log1p(Function):
+    def forward(self, x):
+        return np.log1p(x)
+
+def log10(x):
+    return Log10()(x).set_name(x.name)
+
+def log1p(x):
+    return Log1p()(x).set_name(x.name)
+
+
+#####################################################################
+## FFT functions
+
+class FFT(Function):
+    def forward(self, x):
+        return fft2d.fft2(x)
+
+class FFTShift(Function):
+    def forward(self, x):
+        return fft2d.fftshift(fft2d.fft2(x))
+
+class IFFT(Function):
+    def forward(self, amp, ang):
+        x = amp if ang is None else amp * np.exp(1j * ang)
+        return fft2d.ifft2(x)
+
+class IFFTShift(Function):
+    def forward(self, amp, ang):
+        x = amp if ang is None else amp * np.exp(1j * ang)
+        return fft2d.ifft2(fft2d.ifftshift(x))
+
+class DCT(Function):
+    def forward(self, x):
+        return fft2d.dct(fft2d.dct(x.T, type=2, norm='ortho').T, type=2, norm='ortho')
+
+class IDCT(Function):
+    def forward(self, x):
+        return fft2d.idct(fft2d.idct(x.T, type=2, norm='ortho').T, type=2, norm='ortho')
+
+
+def fft(x):
+    return FFT()(x).set_name(x.name).add_name("fft")
+
+def fftshift(x):
+    return FFTShift()(x).set_name(x.name).add_name("fftshift")
+
+def ifft(amp, ang=None):
+    return IFFT()(amp, ang)
+
+def ifftshift(amp, ang=None):
+    return IFFTShift()(amp, ang)
+
+def dct(x):
+    return DCT()(x).set_name(x.name).add_name("dct")
+
+def idct(x):
+    return IDCT()(x)
+
+
+if __name__ == "__main__":
+
+    x = Image(np.array([
+        [complex(1, 1), complex(2, 2)],
+        [complex(2, 2), complex(1, 1)]
+    ]))
+    x.name = "raw"
+
+    y = x.fft.amplitude
+
+    print(x.data, x.name)
+    print(y.data, y.name)
